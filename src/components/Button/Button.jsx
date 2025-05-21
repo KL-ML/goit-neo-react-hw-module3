ed98fbf8a142873c398c584711a0966828979748
KL-ML <knataly0513@gmail.com>
2025-05-21 17:16:54 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import css from './Button.module.css';
 
-export default function Button({ text, handleClick, variant }) {
+export default function Button({
+  text,
+  variant,
+  tabletDisplay,
+  green,
+  red,
+  addContact,
+  // eslint-disable-next-line no-unused-vars
+  icon: Icon,
+  iconSize,
+  btnType,
+}) {
   return (
     <>
       <button
-        onClick={() => handleClick(text)}
-        className={clsx(css.button, css[variant])}
+        type={btnType}
+        className={clsx(
+          css.button,
+          css[variant],
+          tabletDisplay && css.tabletDisplay,
+          green && css.green,
+          red && css.red,
+          addContact && css.addContact
+        )}
       >
+        <Icon size={iconSize} />
         {text}
       </button>
     </>
